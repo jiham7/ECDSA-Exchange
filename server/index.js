060c58810e49cffba6b69b403bd53ef7faddd964
Jiham Lee <jiham7@gmail.com>
2022-01-22 14:59:01 -0600

    made single public key

--- server/index.js
+++ server/index.js
@@ -1,21 +1,28 @@
+const SHA256 = require('js-sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
 
-const EC = require('elliptic').ec;
-const ec = new EC('secp256k1');
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
+const publicKeys = {}
 const keyPair = {};
-const balances = {}
-const addresses = []
+const balances = {};
+const addresses = [];
 for(let i=0; i<3; i++){
   const key = ec.genKeyPair();
+
+  // Issue is here
+  const publicKey = key.getPublic().encode('hex');
+
   const publicKeyX = key.getPublic().x.toString(16);
   const publicKeyY = key.getPublic().y.toString(16);
   const tuple = [publicKeyX, publicKeyY]
@@ -23,10 +30,11 @@ for(let i=0; i<3; i++){
   console.log("Y: " + publicKeyY);
   const privateKey = key.getPrivate().toString(16);
 
-  addresses.push(tuple);
-  keyPair[tuple] = privateKey;
+  publicKeys[publicKey] = tuple;
+  addresses.push(publicKey);
+  keyPair[publicKey] = privateKey;
 
-  balances[tuple] = 100 - 25*i;
+  balances[publicKey] = 100 - 25*i;
 }
 
 app.get('/balance/:address', (req, res) => {
